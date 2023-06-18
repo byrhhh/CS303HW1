7a746d4396a5a3b1fa03f3a7577578ce95e83049
byrhhh <123522777+byrhhh@users.noreply.github.com>
2023-06-17 23:23:05 -0500

    Create functiondec.h

---
+++ functiondec.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+void ReadFile(int array[150]);  //function declaration to read file
+int NumberCheck(int array[150]);  //function declaration to check if certain integer exists in array return index val
+void ModifyArray(int array[150]);  //function declaration to modify array 
+void AddNumber(int array[150]);   //function declaration to add num to end of array
+void ReplaceArray(int array[150]);  //function declaration to replace any number with 0.
